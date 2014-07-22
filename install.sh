95bca725bb654db4df1f942f39dd26012bc0506b
troyblank <troy@troyblank.com>
2014-07-22 17:17:59 -0500

    removed all installable files

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir plugins
+mkdir platforms
+cordova platform add android
+cordova plugin add https://github.com/Initsogar/cordova-webintent.git
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-inappbrowser.git
+cordova plugin add https://github.com/chariotsolutions/phonegap-nfc
