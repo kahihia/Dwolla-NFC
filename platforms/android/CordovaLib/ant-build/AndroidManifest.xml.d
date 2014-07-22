d911aeae3170680fefd3dca3ce5f9b8b137dd52d
troyblank <troy@troyblank.com>
2014-07-22 14:32:05 -0500

    found more generated files to trash

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\tblank\Desktop\git\_nonDwolla\dwollaNFC\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\tblank\Desktop\git\_nonDwolla\dwollaNFC\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : C:\Users\tblank\Desktop\git\_nonDwolla\dwollaNFC\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
